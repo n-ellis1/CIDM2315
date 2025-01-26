c37f4637c1f4bf2c9e71a37a81e03e4b0af36a6b
n-ellis1 <nellis1@buffs.wtamu.edu>
2025-01-26 16:59:32 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //The linear line equation is Z=4X^2 + 3Y , when X = 2.5, Y = 3.3
+        double X = 2.5;
+        double Y = 3.3;
+
+        double Z = 4*X*X + 3*Y;
+
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.Write("The value of Z is: ");
+        Console.Write(Z);
+    }  
+
+}
