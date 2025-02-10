a8bdb1902e536e7fe8198dbb862499b82369ff9c
n-ellis1 <nellis1@buffs.wtamu.edu>
2025-02-10 16:29:40 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,79 +1,18 @@
-﻿namespace Homework3;
+﻿namespace Homework1;
 
 class Program
 {
     static void Main(string[] args)
     {
-         {
-        // Taking input from the user
-        Console.Write("Input an integer: ");
-        int N = int.Parse(Console.ReadLine());
+        //The linear line equation is Z=4X^2 + 3Y , when X = 2.5, Y = 3.3
+        double X = 2.5;
+        double Y = 3.3;
 
-        // Checking if the number is prime
-        if (IsPrime(N))
-        {
-            Console.WriteLine($"{N} is prime");
-        }
-        else
-        {
-            Console.WriteLine($"{N} is non-prime");
-        }
-    }
+        double Z = 4*X*X + 3*Y;
 
-    static bool IsPrime(int N)
-    {
-        // Handling special case for numbers less than 2
-        if (N < 2)
-        {
-            return false;
-        }
+        Console.WriteLine("X = 2.5, Y = 3.3");
+        Console.Write("The value of Z is: ");
+        Console.Write(Z);
+    }  
 
-        // Loop from 2 to N-1 to check divisibility
-        for (int i = 2; i <= Math.Sqrt(N); i++)
-        {
-            if (N % i == 0)
-            {
-                return false; // Found a divisor, N is not prime
-            }
-        }
-
-        return true; // No divisors found, N is prime
-        }
-
-        
-        //Question 2
-
-         {
-        // Taking input from the user
-        Console.Write("Assign an int value to N: ");
-        int N = int.Parse(Console.ReadLine());
-
-        for (int i = 0; i < N; i++) // Outer loop for rows
-        {
-            for (int j = 0; j < N; j++) // Inner loop for columns
-            {
-                Console.Write("# "); // Print hashtag in each position
-            }
-            Console.WriteLine(); // Move to the next line after each row
-            }
-        }
-
-
-        //Question 3
-
-        {
-        // Taking input from the user
-        Console.Write("Assign an int value to N: ");
-        int N = int.Parse(Console.ReadLine());
-
-        for (int i = 1; i <= N; i++) // i represents the current row
-        {
-            for (int j = 1; j <= i; j++) // j represents the current column
-            {
-                Console.Write("*"); // Print an asterisk
-            }
-            Console.WriteLine(); // Move to the next line after each row
-            }
-        }
-    }
 }
